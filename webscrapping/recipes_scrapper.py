dde39be231d994166725c4384bb1907ee8f4b38d
Pliszka <karoliniakkjk@gmail.com>
2024-06-07 15:35:35 +0200

    Add cleaning ingredient's names

--- webscrapping/recipes_scrapper.py
+++ webscrapping/recipes_scrapper.py
@@ -1,4 +1,6 @@
 import requests
+import re
+from morfeusz2 import Morfeusz
 from bs4 import BeautifulSoup
 
 RECIPIE_SOURCE_URL = "https://www.kwestiasmaku.com/home-przepisy?page=4"
@@ -6,43 +8,59 @@ RECIPIE_BASE_URL = "https://www.kwestiasmaku.com"
 
 class RecipesScrapper:
     def __init__(self):
-        recipe_source = RECIPIE_SOURCE_URL;
+        recipe_source = RECIPIE_SOURCE_URL
         recipie_base_url = RECIPIE_BASE_URL
-        self.__getRecipieDetails();
-        # recipie_names = self.__getRecipieNames();
+        self.__getRecipieDetails()
+        # recipie_names = self.__getRecipieNames()
 
 
     def __getRecipes(self):
-        response = requests.get(RECIPIE_SOURCE_URL);
+        response = requests.get(RECIPIE_SOURCE_URL)
         website_list = response.text;
-        soup = BeautifulSoup(website_list, 'html.parser');
-        return soup.select('div.col-lg-3:has(> div.views-field)');
+        soup = BeautifulSoup(website_list, 'html.parser')
+        return soup.select('div.col-lg-3:has(> div.views-field)')
 
     def __getRecipieDetails(self):
-        recipes = self.__getRecipes();
-        recipies_records = [];
+        recipes = self.__getRecipes()
         for recipie in recipes:
-            soup = BeautifulSoup(str(recipie), 'html.parser');
-            recipie_url = soup.find('a')['href'];
+            soup = BeautifulSoup(str(recipie), 'html.parser')
+            recipie_url = soup.find('a')['href']
             record = {
                 "website_URL": recipie_url,
                 "rating": [],
                 "main_img_URL": soup.find('img')['src'],
                 "ingredients": self.__findIngredients(RECIPIE_BASE_URL+recipie_url),
                 "title": soup.find('span').get_text()
-            };
-            print(record);
+            }
+            print(record)
 
     def __findIngredients(self, url):
-        response = requests.get(url);
-        recipie = response.text;
-        soup = BeautifulSoup(recipie, 'html.parser');
-        ingredients = soup.find("div", class_='field-name-field-skladniki').find_all('li');
+        response = requests.get(url)
+        recipie = response.text
+        soup = BeautifulSoup(recipie, 'html.parser')
+        ingredients = soup.find("div", class_='field-name-field-skladniki').find_all('li')
         all_ingredients = []
+        ingredients = self.__extract_ingredients(ingredients)
         for ingredient in ingredients:
-            all_ingredients.append(ingredient.getText().strip());
-        return all_ingredients;
+            all_ingredients.append(ingredient)
+        return all_ingredients
 
-
-
-
+    def __extract_ingredients(self, all_ingredients):
+        ingredients = []
+        main_pattern = re.compile(
+            r'^(ok\.\s*)?\d+(\s+\d+/\d+)?\s*(szklanki?|łyżeczki?|łyżki?|gramy?|kg|ml|l|duże?|mały?|g|puszka?|puszki?|słoiczek?|słoik?|plasterki)?\s*|szczypta\s*')
+        deko_pattern = re.compile(r'^do\s+dekoracji:\s*', re.IGNORECASE)
+        parenthesis_pattern = re.compile(r'\(.*?\)')
+        colon_pattern = re.compile(r'^.*?:')
+        range_pattern = re.compile(r'\d+\s*-\s*\d+')
+        fraction_pattern = re.compile(r'\d+\s*\/\s*\d+')
+        for item in all_ingredients:
+            original_text = item.getText().strip()
+            clean_item = range_pattern.sub('', original_text).strip()
+            clean_item = main_pattern.sub('', clean_item).strip()
+            clean_item = deko_pattern.sub('', clean_item).strip()
+            clean_item = parenthesis_pattern.sub('', clean_item).strip()
+            clean_item = colon_pattern.sub('', clean_item).strip()
+            clean_item = fraction_pattern.sub('', clean_item).strip()
+            ingredients.append(clean_item)
+        return ingredients
